db90f41d0519157553bcb93ff06f350797fabfa8
4thdraw <4thdraw2@gmail.com>
2024-01-03 13:08:57 +0900

    스타일을 클래스명으로 환원해서 가지고 있는 컴포넌트가 스타일컴포넌트

--- src/Btns.js
+++ src/Btns.js
@@ -1,4 +1,16 @@
-import { Wbtn, Bbtn, BgBanner } from './styled/Commonui'
+import { Wbtn, Bbtn, BgBanner, Wildbanner, Parentwbtn } from './styled/Commonui';
+import styled from 'styled-components';
+
+const Outcom = styled(BgBanner)`
+    background-color: red;
+    color:white;
+    font-size: 50px;
+`
+const Outcom2 = styled(BgBanner)`
+    background-color: yellow;
+    color:white;
+    font-size: 50px;
+`
 
 const Btns = () => {
     const bodybg = {
@@ -12,6 +24,11 @@ const Btns = () => {
                 <h2>큰글씨</h2>
                 <p>작은글씨</p>
             </BgBanner>
+            <Outcom>보통평범한 컴포넌트를 스타일컴포넌트가 가져옴</Outcom>
+            <Outcom2>들어갈 내용과 배경이미지만 바뀌면 얼머나 좋와~</Outcom2>
+            <Wildbanner>스타일컴포넌트그대로</Wildbanner>
+            <Parentwbtn>내안에 Wbtn 클래스 있다</Parentwbtn>
+
             <Wbtn>하얀버튼</Wbtn>
             <Bbtn>검정버튼</Bbtn>
         </div>
--- src/styled/Commonui.js
+++ src/styled/Commonui.js
@@ -20,10 +20,24 @@ export const Bbtn = styled.button`
       line-height: 2rem;
       text-align: center;  
 `
+//스타일컴포넌트 생성
+//1. 이름은 Wildbanner
+//2. 태그는 div로 출력
+// 3. 배경색이 검정, 글자 하얀색, 가운데 정렬, 위아래 여백은 200
+export const Wildbanner = styled.div`
+background-color: yellow;
+color: white;
+text-align: center;
+padding: 200px 0;
+`;
+
+export const Parentwbtn = styled(Wbtn)`
+         border:10px solid green;
+`
 
 export const BgBanner = (props) => {
     return (
-        <div className={props.className} >
+        <div className={props.className}  >
             {props.children}
         </div>
     )
