672712bc11f23c84a8ba0a7cfd159a7b91b8eb22
4thdraw <4thdraw2@gmail.com>
2024-01-03 11:55:38 +0900

    스타일컴포넌트로 재사용쵝오

---
+++ src/styled/Commonui.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const Wbtn = styled.button`
+      background-color: black;
+      color: white;
+      border: 1px solid #fff;
+      border-radius: 30px;
+      display: inline-block;
+      width:100px;
+      line-height: 2rem;
+      text-align: center;  
+`
+export const Bbtn = styled.button`
+      background-color: white;
+      color: black;
+      border: 1px solid #000;
+      border-radius: 30px;
+      display: inline-block;
+      width:100px;
+      line-height: 2rem;
+      text-align: center;  
+`
+
